5e03e3fbaa04e8a20ca104e978fb0250460cb80b
Martyn Whitwell <martyn.whitwell@gmail.com>
2013-04-29 13:26:37 +0100

    Working on login page; added fancybox library, moved old javascripts

--- app/assets/classes/util.class.php
+++ app/assets/classes/util.class.php
@@ -65,13 +65,23 @@
 			return $o;	
 		}
 		
-		public static function userBox(user &$u, view &$superView){
-			if($u->getId() != null){
-				$superView->replace("userBox", "<div id=\"loggedIn\"><div id=\"user\" style=\"float:left\"><a href=\"/profile\"><img src='{$u->getPicture()}' width='30' height='30' align='left' alt='User profile picture' /> {$u->getName()}</a> (<a href=\"/auth/logout\">Logout</a>)</div></div>");			
-			} else {
-				$superView->replace("userBox", new view('frag.login'));
-			}
-		}
+        //<li class='sign-in-link'><a href='login.html'>SIGN IN / REGISTER</a></li>
+        
+        public static function userBox(user &$u, view &$superView){
+            if($u->getId() != null){
+                $superView->replace("userBox", "<div id=\"loggedIn\"><div id=\"user\" style=\"float:left\"><a href=\"/profile\"><img src='{$u->getPicture()}' width='30' height='30' align='left' alt='User profile picture' /> {$u->getName()}</a> (<a href=\"/auth/logout\">Logout</a>)</div></div>");            
+            } else {
+                $superView->replace("userBox", new view('frag.login'));
+            }
+        }
+        
+		//public static function userBoxOLD(user &$u, view &$superView){
+		//	if($u->getId() != null){
+		//		$superView->replace("userBox", "<div id=\"loggedIn\"><div id=\"user\" style=\"float:left\"><a href=\"/profile\"><img src='{$u->getPicture()}' width='30' height='30' align='left' alt='User profile picture' /> {$u->getName()}</a> (<a href=\"/auth/logout\">Logout</a>)</div></div>");			
+		//	} else {
+		//		$superView->replace("userBox", new view('frag.login'));
+		//	}
+		//}
 		
 		public static function addScripts(array $scripts){
 			$out = "";
---
+++ presentation/scripts/co-design.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	//turn on fancybox for login
+	$('.fancybox').fancybox();
+	
+/*		alert('test0');
+		$('#loginShow, .loginButton').bind('click', function(){
+			alert('test1A');
+			 $.get('/home/login', function(data){
+			 	alert('test1B');
+			 	$('#loginForm').html(data);
+			 	$('#username').focus();
+			 });
+		});
+		
+		$('#registerShow, .registerButton').bind('click', function(){
+			alert('test2A');
+			 $.get('/home/register', function(data){
+			 	alert('test2B');
+			 	$('#contentPane').html(data);
+			 	$('#username').focus();
+			 });
+		});
+*/
+});	
