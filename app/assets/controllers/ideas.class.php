3a0befdedcedad169e3d9c689a02115d46782126
Emanuil Tolev <emanuil.tolev@gmail.com>
2013-05-02 19:25:54 +0100

    work on the individual ideas page, but involving changes to ideas controller and frag.comment view

--- app/assets/controllers/ideas.class.php
+++ app/assets/controllers/ideas.class.php
@@ -197,7 +197,8 @@ class controller_ideas extends controller {
 			$this->m_owner = true;
 			$this->viewport()->replace('owner', "you");
 		} else {
-			$this->viewport()->replace('owner', $this->m_ideaOwner->getHTMLName());		
+			$this->viewport()->replace('owner', $this->m_ideaOwner->getHTMLName());
+			$this->viewport()->replace('owner_text_name', $this->m_ideaOwner->getName());
 		}
 
 		$this->viewport()->replace('ownerPic', $this->m_ideaOwner->getPicture());
